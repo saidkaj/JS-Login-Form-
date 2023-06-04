0b569b9f7635096718708d89660a93a65f9aa122
Said Jahangirov <saidjahangirov@Saids-MacBook-Air.local>
2023-06-05 03:53:09 +0400

    created chess and login forms

---
+++ script.js
@@ -0,0 +1,18 @@
+let chessboard = document.getElementById("chessBoard");
+
+let letter = "ABCDEFGH";
+
+let boardIndex = "";
+
+for (let i = 0; i < 9; i++) {
+    boardIndex += `<tr>`;
+    for (let j = 0; j < 9; j++) {
+        boardIndex += `<td class = "${(i + j) % 2 ? "black" : "white"}">${i == 0 ? letters[j] :
+            j == 0 ? i : ''
+            }</td>`;
+    }
+    boardIndex += "</tr>";
+}
+
+chessboard.innerHTML = boardIndex;
+
